2ecc920e43887601761db0f94a412cd9cafad6e2
MaciejKsi <125036778+MaciejKsi@users.noreply.github.com>
2023-03-16 16:12:15 +0100

    user nie działający

--- client/app.js
+++ client/app.js
@@ -2,13 +2,31 @@ function checkLogin() {
   const login = document.getElementById("login").value;
   const password = document.getElementById("password").value;
 
-  const user = "admin";
+  const admin = "admin";
   const adminPass = "admin";
 
-  if (login == user && password == adminPass) {
+  if (login == admin && password == adminPass) {
     console.log("zalogowano");
     localStorage.setItem("login", "admin");
     window.location.href = "admin.html";
+  } else {
+    console.log("błędny login lub hasło");
+    alert("błędny login lub hasło");
+    localStorage.setItem("login", "false");
+  }
+}
+
+function checkLogin2() {
+  const login = document.getElementById("login").value;
+  const password = document.getElementById("password").value;
+
+  const user = "user";
+  const userPass = "user";
+
+  if (login == user && password == userPass) {
+    console.log("zalogowano");
+    localStorage.setItem("login", "user");
+    window.location.href = "user.html";
   } else {
     console.log("błędny login lub hasło");
     alert("błędny login lub hasło");
@@ -20,6 +38,14 @@ function checkAdmin() {
   const admin = localStorage.getItem("login");
 
   if (admin != "admin") {
+    window.location.href = "login.html";
+  }
+}
+
+function checkUser() {
+  const user = localStorage.getItem("login");
+
+  if (user != "user") {
     window.location.href = "login.html";
   }
 }
