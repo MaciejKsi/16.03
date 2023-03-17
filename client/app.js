46372c7a65b9c89b1755fce8f40ce44cb9f82cea
MaciejKsi <125036778+MaciejKsi@users.noreply.github.com>
2023-03-17 16:08:40 +0100

    a

--- client/app.js
+++ client/app.js
@@ -7,5 +7,27 @@ async function getUser() {
 
   if (json.user != undefined) {
     localStorage.setItem("upr", JSON.stringify(json));
+  } else {
+    localStorage.setItem("upr", "false");
+  }
+}
+
+function checkUser() {
+  const user = JSON.parse(localStorage.getItem("upr"));
+
+  const url = window.location.href;
+
+  if (user.upr != "admin" && url.includes("admin.html")) {
+    window.location.href = "index.html";
+  }
+}
+
+function checkUs() {
+  const user = JSON.parse(localStorage.getItem("upr"));
+
+  const url = window.location.href;
+
+  if (user.upr != "user" && url.includes("user.html")) {
+    window.location.href = "index.html";
   }
 }
